944076ed3ac7b249cd0ce804bf4b455dc06b39a4
thecplusplus <thecplusplus.2611@gmail.com>
2020-08-31 12:20:15 +0530

    Final commit

---
+++ products/urls.py
@@ -0,0 +1,10 @@
+
+from django.urls import path, include
+from . import views
+urlpatterns = [
+    
+    path('create',views.create,name='create'),
+    path('<int:product_id>/',views.details,name='details'),
+   path('<int:product_id>/upvote', views.upvote, name='upvote'),
+   
+]
--- products/views.py
+++ products/views.py
@@ -1,5 +1,51 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
+from .models import Product
+from django.utils import timezone
+from django.shortcuts import get_object_or_404
 
 # Create your views here.
 def home(request):
-    return render(request,'products/home.html')
+    products=Product.objects
+    return render(request,'products/home.html',{'products':products})
+    
+
+@login_required(login_url="/accounts/signup")
+def create(request):
+    if request.method == 'POST':
+        if request.POST['title'] and request.POST['body'] and request.POST['url']:
+            product = Product()
+            try:
+                product.icon = request.FILES['icon']
+                product.image = request.FILES['image']
+            except Exception as error:
+                return render(request, 'products/create.html',{'error':'Image or icon is not uploaded!!.'})
+            product.title = request.POST['title']
+            product.body = request.POST['body']
+            if request.POST['url'].startswith('http://') or request.POST['url'].startswith('https://'):
+                product.url = request.POST['url']
+            else:
+                product.url = 'http://' + request.POST['url']
+            product.icon = request.FILES['icon']
+            product.image = request.FILES['image']
+            product.pub_date = timezone.datetime.now()
+            product.hunter = request.user
+            product.save()
+            return redirect('/products/'+str(product.id))
+        else:
+            return render(request, 'products/create.html',{'error':'All fields are required.'})
+    else:
+        return render(request, 'products/create.html')
+
+def details(request, product_id):
+    prod = get_object_or_404(Product,pk = product_id)
+    return render(request,'products/details.html',{'prod':prod})
+
+@login_required(login_url="/accounts/signup")
+def upvote(request, product_id):
+    if request.method == 'POST':
+        product = get_object_or_404(Product, pk=product_id)
+        product.votes_total += 1
+        product.save()
+        return redirect('/products/' + str(product.id))
+    
